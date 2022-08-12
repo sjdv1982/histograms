3bae987c1df9e45697144b1645d4b1065956ae23
Sjoerd de Vries <sjdv1982@gmail.com>
2022-08-12 10:16:18 +0200

    updated example with rescoring

--- 1M5K/run.sh
+++ 1M5K/run.sh
@@ -9,3 +9,5 @@ mkdir -p distance-arrays
 python3 ../scripts/build-distance-arrays.py . --distances 3 10 --discretizations 3 0.5 --rmsd-thresholds 5 15 --rank-chunk 99999999
 mkdir -p histograms
 python3 ../scripts/make-histograms.py distance-arrays 50 histograms/
+python3 ../scripts/score-with-all-histograms.py . CAC histograms/ > CAC.score
+paste CAC.rmsd CAC.score | sort -nrk2 | python3 -c 'import sys; from matplotlib import pyplot as plt; import numpy as np; d = np.loadtxt(sys.stdin); plt.scatter(*(d.T),s=0.1); plt.show()'
